0cabcb8543ab8f7b120f508692bd8a3e23d4dce9
Ashish-Jewargikar <ashish961169@gmail.com>
2023-03-14 14:42:26 +0530

    first commit

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -8,22 +8,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RestController
 @RequestMapping("/blogs")
 public class BlogController {
 
+    @Autowired
+    BlogService blogService;
+
     @PostMapping
-    public ResponseEntity createBlog(@RequestParam Integer userId ,
-                                     @RequestParam String title,
-                                     @RequestParam String content) {
+    public ResponseEntity createBlog(@RequestParam Integer userId , @RequestParam String title, @RequestParam String content) {
         // Create a blog and add it under given user
+        blogService.createAndReturnBlog(userId,title,content);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{blogId}")
     public ResponseEntity<Void> deleteBlog(@PathVariable int blogId) {
         // Delete the blog using deleteById
+        blogService.deleteBlog(blogId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
